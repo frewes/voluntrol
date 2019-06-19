1dbcffa969adec043025f0dce21c50e5295a7016
Fred Westling <fredrik.westling@gmail.com>
2019-06-19 12:02:46 +1000

    Added basic boilerplate user storage

--- public/electron.js
+++ public/electron.js
@@ -5,11 +5,25 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const isDev = require('electron-is-dev');
 
+const Store = require('../src/store.js');
+
 let mainWindow;
 
+const store = new Store({
+    configName: 'user-preferences',
+    defaults: {
+        windowBounds: {width: 800, height: 600 }
+    }
+});
+
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 900, height: 680});
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    let {width, height} = store.get('windowBounds');
+    mainWindow = new BrowserWindow({width: width, height: height});
+    mainWindow.on('resize', () => {
+        let { width, height } = mainWindow.getBounds();
+        store.set('windowBounds', {width, height}); 
+     });
+     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     if (isDev) {
         mainWindow.webContents.openDevTools();
     }
